912d819ad61b29ce415b9a6d0c74fa75ca332b3c
Sagar Barapatre <b119016@iiit-bh.ac.in>
2020-10-05 08:06:58 +0530

    Add files via upload

---
+++ Stack/1) stack_implementation_using_linkedlist.cpp
@@ -0,0 +1,67 @@
+// C++ program for linked list implementation of stack
+#include <bits/stdc++.h>
+using namespace std;
+
+// A structure to represent a stack
+class StackNode {
+public:
+	int data;
+	StackNode* next;
+};
+
+StackNode* newNode(int data)
+{
+	StackNode* stackNode = new StackNode();
+	stackNode->data = data;
+	stackNode->next = NULL;
+	return stackNode;
+}
+
+int isEmpty(StackNode* root)
+{
+	return !root;
+}
+
+void push(StackNode** root, int data)
+{
+	StackNode* stackNode = newNode(data);
+	stackNode->next = *root;
+	*root = stackNode;
+	cout << data << " pushed to stack\n";
+}
+
+int pop(StackNode** root)
+{
+	if (isEmpty(*root))
+		return INT_MIN;
+	StackNode* temp = *root;
+	*root = (*root)->next;
+	int popped = temp->data;
+	free(temp);
+
+	return popped;
+}
+
+int peek(StackNode* root)
+{
+	if (isEmpty(root))
+		return INT_MIN;
+	return root->data;
+}
+
+int main()
+{
+	StackNode* root = NULL;
+
+	push(&root, 10);
+	push(&root, 20);
+	push(&root, 30);
+
+	cout << pop(&root) << " popped from stack\n";
+
+	cout << "Top element is " << peek(root) << endl;
+
+	return 0;
+}
+
+// This is code is contributed by rathbhupendra
