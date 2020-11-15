4b0796acbd633b1366a44eb3d1bef48f2e43af08
Sagar Barapatre <b119016@iiit-bh.ac.in>
2020-11-15 22:55:05 +0530

    Add files via upload

---
+++ Recursion/Tower_of_Hanoi.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+void towerOfHanoi(int n,char src,char dest,char helper)
+{
+    if(n==0)return;
+
+    towerOfHanoi(n-1,src,helper,dest);
+
+    cout<<"Move "<<n<<" disk to "<<src<<" to "<<dest<<endl;
+
+    towerOfHanoi(n-1,helper,dest,src);
+}
+int main()
+{
+    int n;
+    cin>>n;
+
+    towerOfHanoi(n,'A','C','B');
+}
